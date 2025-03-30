d63f1f3e55f31f450d831f17c5fc0f0fc4eb559f
Moin Teng <solitude316@gmail.com>
2025-03-30 20:41:32 +0800

    Integrate With CoreUI

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Web.Models;
 
@@ -8,14 +9,27 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IWebHostEnvironment _environment;
+
+    public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
     {
         _logger = logger;
+        _environment = environment;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var indexPath = _environment.WebRootFileProvider.GetFileInfo("index.html");
+
+        if (!indexPath.Exists)
+        {
+            return NotFound("index.html not found in wwwroot folder.");
+        }
+        var content = 
+
+        ViewBag.HtmlContent = System.IO.File.ReadAllText(indexPath.PhysicalPath!);
+
+        return PartialView();
     }
 
     public IActionResult Privacy()
