5e02ef7f0fea407d22b59e9b3e1e8c6fed1acc98
Moin Teng <solitude316@gmail.com>
2025-06-03 17:51:11 +0000

    剛剛建好

---
+++ src/API/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    public HomeController()
+    {
+
+    }
+
+    [HttpGet("hello")]
+    public IActionResult Get()
+    {
+        return Ok("Hello, World!");
+    }
+
+    [HttpGet("hello2")]
+    public IActionResult Get2()
+    {
+        return Ok("Hello, World 2!");
+    }
+
+}
